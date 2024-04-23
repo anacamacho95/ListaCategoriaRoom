11032ae502988a3f50de03c27ca5f47282fc323e
anacamacho95 <ana.camacho.sanchez.alu@iesjulioverne.es>
2024-04-23 14:43:13 +0200

    mas cambios

--- app/src/main/java/com/example/listacategoriaroom/entidades/Categoria.kt
+++ app/src/main/java/com/example/listacategoriaroom/entidades/Categoria.kt
@@ -1,7 +1,8 @@
 package com.example.listacategoriaroom.entidades
 
+import androidx.room.Entity
 import java.io.Serializable
-
+@Entity(tableName = "Categoria")
 class Categoria ( var nombre: String): Serializable {
     //Una categoria tiene muchas tareas
     var tareas : MutableList<Tarea> = mutableListOf()
--- app/src/main/java/com/example/listacategoriaroom/interfaces/InterfaceDaoTareas.kt
+++ app/src/main/java/com/example/listacategoriaroom/interfaces/InterfaceDaoTareas.kt
@@ -1,31 +1,44 @@
 package com.example.listacategoriaroom.interfaces
 
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Update
 import com.example.listacategoriaroom.entidades.Categoria
 import com.example.listacategoriaroom.entidades.Item
 import com.example.listacategoriaroom.entidades.Tarea
 
 interface InterfaceDaoTareas :InterfaceDao{
     //CRUD TAREAS
-    //crear
-    fun addTarea (ca: Categoria, ta: Tarea)
+    @Insert
+    fun addTarea (idCategoria: Int, ta: Tarea)
+
     //leer Todas las tareas
-    fun getTareas(ca: Categoria): MutableList<Tarea>
-    //actualizar
-    fun updateNombreTarea(ca: Categoria, taAnt: Tarea, taNue: Tarea)
-    //borrar
-    fun deleteTarea (ca: Categoria, ta: Tarea)
+    @Query("SELECT * FROM Tarea WHERE idCategoria LIKE :idCat")
+    fun getTareas(idCat: Int): MutableList<Tarea>
+
+    @Update
+    fun updateNombreTarea(idCat: Int, taAnt: Tarea, taNue: Tarea)
+
+    @Delete
+    fun deleteTarea (idCat: Int, ta: Tarea)
 
     //CRUD ITEMS
-    //crear
+    @Insert
     fun addItem (ca: Categoria, ta: Tarea, ite: Item)
+
     //leer Todos los items
+    @Query("SELECT * FROM Item WHERE Tarea LIKE :ta AND Categoria LIKE :ca")
     fun getItems(ca: Categoria, ta: Tarea): MutableList<Item>
-    //actualizar item
+
+    @Update
     fun updateItem(ca: Categoria, ta: Tarea,iteAnt: Item, iteNue: Item)
-    //borrar
+
+    @Delete
     fun deleteItem (ca: Categoria ,ta: Tarea, ite: Item)
 
     //NºItems
+    @Query("SELECT COUNT(*) FROM Item WHERE idTarea = :taId AND idCategoria = :caId")
     fun getNItems(ca: Categoria, ta: Tarea): Int
 
 }
